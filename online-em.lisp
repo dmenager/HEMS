f60ac2ea31d5131aabbd0b711cb676bc6f932eb5
dmenager <david.menager@parallaxresearch.org>
2026-06-26 11:53:06 -0400

    reverted step size back to 1.o

--- online-em.lisp
+++ online-em.lisp
@@ -640,7 +640,7 @@ eta_t times the current posterior ESS."
 
 (defun online-em-step (bn latent-vars datum
                         &key
-                          (step-size .10d0)
+                          (step-size 1.0d0)
                           (lr 1.0d0)
                           (equivalent-sample-size 1.0d0)
                           (latent-perturbation 5.0d-2)
